faf05791cc0636070c77542556340f905548bfba
Daniel De La Rosa <dela1000@gmail.com>
2023-12-13 14:55:24 -0600

    Update ImageCarousel.tsx

--- src/components/ImageCarousel/ImageCarousel.tsx
+++ src/components/ImageCarousel/ImageCarousel.tsx
@@ -56,20 +56,22 @@ const ImageCarousel = ({ images, onClose }: ImageCarouselProps) => {
 
   return (
     <div className="font-sans fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="relative mx-4 my-4 shadow-lg flex flex-col items-center overflow-hidden">
+      <div className="relative mx-4 my-4 flex flex-col items-center overflow-hidden">
         <div className="flex flex-col items-center w-full h-full">
-          <div className="mx-14 my-2">
-            <img
-              key={nextIndex}
-              src={images[currentIndex].image}
-              alt={`Slide ${currentIndex + 1}`}
-              className={`transition-opacity duration-500 ease-in-out ${isImageLoading ? 'opacity-0' : 'opacity-100'}`}
-              style={{
-                maxHeight: `calc(100vh - ${extraVerticalSpace}px - ${margin * 2}px)`,
-                maxWidth: `calc(100vw - ${margin * 2}px)`,
-              }}
-              onLoad={() => setIsImageLoading(false)}
-            />
+          <div className="mx-14 my-2 shadow-lg">
+            <div className="flex justify-center">
+              <img
+                key={nextIndex}
+                src={images[currentIndex].image}
+                alt={`Slide ${currentIndex + 1}`}
+                className={`transition-opacity duration-500 ease-in-out ${isImageLoading ? 'opacity-0' : 'opacity-100'}`}
+                style={{
+                  maxHeight: `calc(100vh - ${extraVerticalSpace}px - ${margin * 2}px)`,
+                  maxWidth: `calc(100vw - ${margin * 2}px)`,
+                }}
+                onLoad={() => setIsImageLoading(false)}
+              />
+            </div>
             <p className="font-garamond w-full mt-2 md:p-2 p-4 bg-white text-center">{images[currentIndex].title}</p>
           </div>
         </div>
