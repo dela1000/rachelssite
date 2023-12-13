5b75970f641dd22177d194787b0fff5cfba92f38
Daniel De La Rosa <dela1000@gmail.com>
2023-12-13 16:29:50 -0600

    Update Portfolio.tsx

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -17,16 +17,18 @@ const Portfolio = () => {
   };
 
   return (
-    <div className="flex flex-wrap justify-center items-center px-4 pb-14">
+    <div className="flex flex-wrap justify-center">
       {carouselOpen && <ImageCarousel images={selectedImages} onClose={handleCloseCarousel} />}
 
-      {portfolioItems.map((item, index) => (
-        <div key={index} className="group relative m-3 xl:w-1/2 lg:w-1/3" onClick={() => handleItemClick(item.carouselImages)}>
-          <div className="xl:flex xl:h-screen">
-            <img src={item.imageSrc} alt={item.altText} className="object-cover drop-shadow-md" />
+      {portfolioItems.map((item) => (
+        <div key={item.text} className="group relative m-1 w-1/2 xl:w-1/3" onClick={() => handleItemClick(item.carouselImages)}>
+          <div className="flex aspect-square w-full justify-center overflow-hidden xl:h-screen">
+            <img src={item.imageSrc} alt={item.altText} className="h-full w-full object-cover xl:w-auto drop-shadow-md" />
           </div>
           <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-0 group-hover:bg-opacity-50 transition">
-            <p className="text-white xl:text-black text-4xl font-bold text-center group-hover:text-white drop-shadow-md transition">{item.text}</p>
+            <p className="text-white xl:text-black text-3xl lg:text-4xl font-bold text-center group-hover:text-white drop-shadow-md transition">
+              {item.text}
+            </p>
           </div>
         </div>
       ))}
