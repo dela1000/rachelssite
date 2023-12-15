a8385384f7435b76df707e5d89b33a40968a9740
Daniel De La Rosa <dela1000@gmail.com>
2023-12-15 12:51:59 -0600

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,12 +5,12 @@ const Footer = () => {
   return (
     <footer className="sticky bottom-0 p-6 bg-white">
       <div className="flex items-end justify-between">
-        <div className="w-8 h-8">
+        <div className="w-8 h-8 pr-2">
           <a href="https://www.linkedin.com/in/rachel-whitener/" target="_blank" rel="noopener noreferrer">
             <img src={linkedin} alt="linkedin" />
           </a>
         </div>
-        <div className="lg:text-sm text-xs">{year} - Rachel Whitener. All rights reserved</div>
+        <div className="pb-1.5 lg:text-sm text-xs">{year} - Rachel Whitener. All rights reserved</div>
       </div>
     </footer>
   );
