1ae25f47f60af6c535a88a89e827b9b8ee052654
Daniel De La Rosa <dela1000@gmail.com>
2023-12-15 12:55:03 -0600

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import linkedin from 'src/assets/linkedin-icon.png';
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
-    <footer className="sticky bottom-0 p-6 bg-white">
+    <footer className="sticky bottom-0 pt-4 pb-2 bg-white">
       <div className="flex items-end justify-between">
         <div className="w-8 h-8 pr-2">
           <a href="https://www.linkedin.com/in/rachel-whitener/" target="_blank" rel="noopener noreferrer">
